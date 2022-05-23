c3546635904706959e88f9b46e4e38d681ad1f7b
AbinayaSakthikumar <abinayaabi12396@gmail.com>
2022-05-23 15:19:44 +0530

    End of the section 8 (86.Extending the user entity)

---
+++ Entities/photo.cs
@@ -0,0 +1,11 @@
+namespace API.Entities
+{
+    public class photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+
+    }
+}
